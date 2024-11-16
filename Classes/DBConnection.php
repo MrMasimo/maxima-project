4be790d94028e369049de04452d4ab2bc6f11e87
Maksim Mamontov <80010074+MrMasimo@users.noreply.github.com>
2024-11-17 00:29:53 +0300

    pattern Singleton

--- Classes/DBConnection.php
+++ Classes/DBConnection.php
@@ -2,32 +2,57 @@
 
 namespace Classes;
 
+use Exception;
+
 /**
- * Класс для создания подключения к базе данных
+ * Класс для создания подключения к базе данных - паттерн Singleton
  */
 final class DBConnection
-{
+{   
+    private static $instance = null;
     /**
      * Подключение к базе данных и возврат экземпляра объекта \PDO
      * @return \PDO
      * @throws \Exception
      */
-    public function connect()
+    private function connect()
     {
-        
+    }
+
+    protected function __construct()
+    {
     }
 
     /**
      * возврат экземпляра объекта DBConnection
      * тип @return
      */
+    private static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @throws Exception
+     */
     public static function get()
     {
-
+        return self::getInstance()->connect();
     }
 
-    protected function __construct()
+
+    protected function __clone()
     {
-
+        throw new Exception("Cannot clone singleton");
     }
+
+    protected function __wakeup()
+    {
+        throw new Exception("Cannot unserialize singleton");
+    }
+
 }
